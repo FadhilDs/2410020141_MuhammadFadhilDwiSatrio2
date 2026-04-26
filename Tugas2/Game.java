6cc42e62a2bcb2ef77fe078a7abcb68b3e07abf0
FadhilDs <mfadhilkill123@gmail.com>
2026-04-26 19:12:05 +0700

    Reformat Game.java with consistent spacing

--- Tugas2/Game.java
+++ Tugas2/Game.java
@@ -5,26 +5,26 @@ public class Game {
     String namaKarakter;
     int level;
 
-    // Constructor tanpa parameter
+    
     public Game() {
         System.out.println("Game Dimulai");
         namaKarakter = "Balmond";
         level = 15;
     }
 
-    // Constructor dengan parameter
+    
     public Game(String namaKarakter, int level) {
         this.namaKarakter = namaKarakter;
         this.level = level;
     }
 
-    // Method tanpa return
+    
     public void tampilData() {
         System.out.println("Karakter: " + namaKarakter);
         System.out.println("Level: " + level);
     }
 
-    // Method dengan return
+   
     public String getNamaKarakter() {
         return namaKarakter;
     }
